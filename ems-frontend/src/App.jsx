338c4a534ecda0b2514f43cf4b6de57144f60e1b
giritharan <giritharan.cbe@gmail.com>
2026-02-08 13:39:19 +0530

    code refactoring

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -11,7 +11,6 @@ function App() {
   return (
     <>
       <BrowserRouter>
-
         <HeaderComponent/>
         <Routes>
           <Route path ='/' element = {<EmployeeTableComponent/>}></Route>
@@ -19,8 +18,6 @@ function App() {
           <Route path ='/add-employee' element = {<AddEmployee/>}></Route>
           <Route path ='/update-employee/:id' element = {<AddEmployee/>}></Route>
         </Routes>
-        
-
         <FooterComponent/>
       </BrowserRouter>
     </>
